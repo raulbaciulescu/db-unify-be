1f052a7b99a3a241bd57c32ae97a0126fc9b99ae
raulbaciulescu <raulbaciulescu@gmail.com>
2025-04-25 12:00:10 +0300

    first commit

---
+++ src/main/java/com/raulb/db_unify_be/DbUnifyBeApplication.java
@@ -0,0 +1,13 @@
+package com.raulb.db_unify_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbUnifyBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbUnifyBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raulb/db_unify_be/DbUnifyBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raulb.db_unify_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbUnifyBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
